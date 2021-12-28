9f3ca21bc8c18b2af6c3be692c92217004b051b5
Ivan Petrovic <mr.petrovicivan@gmail.com>
2021-12-28 20:31:23 +0100

    maven update

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         window = primaryStage;
         window.setTitle("UI_2021");
         Scene scene = new Scene(new LoginView(window), 300, 200);
